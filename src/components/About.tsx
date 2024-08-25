0bdda38d92acaa93d0edf1bc9ed91a29bbe6d131
summer <3345618+shiwanjun@users.noreply.github.com>
2024-08-25 19:37:13 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ export const About = () => {
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
+                  关于{" "}
                 </span>
                 Company
               </h2>
@@ -27,7 +27,7 @@ export const About = () => {
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
+                amet, consectetur adipiscing elit. --- test
               </p>
             </div>
 
